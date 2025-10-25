9dac109ec7d1464c49fc37949dbfa005d41391ee
sungeunp <sungeun1214@gmail.com>
2025-10-25 14:27:36 +0900

    feat(parse): 각 단계 로직 구현 (1)
    - 각 단계별 실제 로직을 구현한다.
      문자열을 입력받아 제한 사항을 적용하여 ',' 기준 문자열 parsing을 진행한다.
    - 제한 사항을 위반한 경우 exception을 발생시켜, process를 통한 program exit이 아닌 예외를 통한 gracefully-exit을 적용했다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import { Console } from "@woowacourse/mission-utils";
 class App {
+  static ERROR_TITLE = "[ERROR]";
+
+  // 사용자가 입력한 law car names string을 restriction을 적용하여 배열로 validate 합니다.
+  static restriction_carLength = 5;
+  validateCarNames(carNames) {
+    if (!carNames || typeof carNames !== "string") return [];
+
+    const splitedResult = carNames.split(",");
+    return splitedResult.filter((item) => {
+      if (item.length < 1) {
+        throw new Error(
+          `${App.ERROR_TITLE} 경주할 자동차의 이름이 비어있습니다.`
+        );
+      }
+      if (item.length > App.restriction_carLength) {
+        throw new Error(
+          `${App.ERROR_TITLE} 경주할 자동차의 이름이 5자가 넘습니다. (${item})`
+        );
+      }
+      return item.trim() !== "";
+    });
+  }
+
   async run() {
-    const unsafe_carNames = await Console.readLineAsync(
+    const unsafeCarNames = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+    const safeCarNames = this.validateCarNames(unsafeCarNames);
+    // Validate for `safeCarNames`
+    if (safeCarNames.length < 0) {
+      throw new Error(`${App.ERROR_TITLE} 경주할 자동차가 없습니다.`);
+    }
 
     const unsafe_iterationCount = await Console.readLineAsync(
       "시도할 횟수는 몇 회인가요?\n"
