a9a8fb0a33b93cb0244af3ed697ed82f187d134e
sungeunp <sungeun1214@gmail.com>
2025-10-25 13:51:38 +0900

    feat(skeleton): 입출력 스켈레톤 구현
    - 프로그램의 전체적인 입출력 스켈레톤을 구현한다.
    
    1. 경주에 참가하는 자동차 이름을 입력받는다.
    2. 시도할 횟수를 입력받는다.
    3. 실행 결과를 출력한다.
    4. 실행 결과에 따른 최종 우승자를 출력한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
 class App {
-  async run() {}
+  async run() {
+    const unsafe_carNames = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+
+    const unsafe_iterationCount = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?\n"
+    );
+
+    Console.print("\n실행 결과");
+
+    Console.print(`최종 우승자 : pobi, jun`);
+  }
 }
 
 export default App;
